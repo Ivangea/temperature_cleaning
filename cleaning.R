29450aa3b38028b5b01db295e2cdd60d42a44d94
Ivan GA <ivan_2c@hotmail.com>
2018-12-18 11:34:28 -0600

    Function conditional observatories

--- cleaning.R
+++ cleaning.R
@@ -22,7 +22,6 @@ df$date<- with(df, ymd_h(paste(FECHA, HORA, sep= ' ')))
 df<-df[,c(which(colnames(df)=="date"),which(colnames(df)!="date"))]
 df[df == -99] <- NA
 
-
 l = lapply(c("high", "low", "mean"), function(type)
 {temp <- timeAverage(df[-2:-4], avg.time = "day",
                      statistic = list(mean = "mean", low = "min", high = 
@@ -33,23 +32,10 @@ colnames(temp)[colnames(temp) == 'value'] <- paste0(type, ".temp")
 temp})
 df_temp <- left_join(l[[1]], l[[2]], by = c("date", "variable")) %>%
   left_join(l[[3]], by = c("date", "variable"))
+colnames(df_temp)[colnames(df_temp) == 'variable'] <- 'cve'
 
-#df_temp.mean<- timeAverage(df[-2:-4], avg.time = "day", data.thresh = 75)
-#df_temp.mean<- melt(df_temp.mean, id.vars = "date")
-#colnames(df_temp.mean)[colnames(df_temp.mean) == 'value'] <- 'temp.mean'
-#df_temp.low<- timeAverage(df[-2:-4], avg.time = "day", statistic = "min", data.thresh = 75)
-#df_temp.low<- melt(df_temp.low, id.vars = "date")
-#colnames(df_temp.low)[colnames(df_temp.low) == 'value'] <- 'low.temp'
-#df_temp.hi<- timeAverage(df[-2:-4], avg.time = "day", statistic = "max", data.thresh = 75)
-#df_temp.hi<- melt(df_temp.hi, id.vars = "date")
-#colnames(df_temp.hi)[colnames(df_temp.hi) == 'value'] <- 'hi.temp'
-#df_temp <- left_join(df_temp.hi, df_temp.low, by = c("date", "variable")) %>%
-#  left_join(df_temp.mean, by = c("date", "variable"))
-#colnames(df_temp)[colnames(df_temp) == 'variable'] <- 'cve'
-#df_temp$cve<-as.character(df_temp$cve)
-#rm(df_temp.hi, df_temp.low, df_temp.mean)
 
-#  Relative humidity___________________________________________________________________________________________________#
+#  Relative humidity_______________________________________________________________________________________________#
 temp = list.files(pattern="*RH.xls")
 myfiles = lapply(temp, read_excel)
 df2 <- bind_rows(myfiles, .id = "id")
@@ -64,7 +50,7 @@ colnames(df_r.humidity)[colnames(df_r.humidity) == 'variable'] <- 'cve'
 df_r.humidity$cve<-as.character(df_r.humidity$cve)
 df_REDMET<-left_join(df_temp, df_r.humidity, by = c("date","cve"))
 
-# Wind speed___________________________________________________________________________________________________#
+# Wind speed_______________________________________________________________________________________________________#
 temp = list.files(pattern="*WSP.xls")
 myfiles = lapply(temp, read_excel)
 df3 <- bind_rows(myfiles, .id = "id")
@@ -79,7 +65,7 @@ colnames(df_windsp)[colnames(df_windsp) == 'variable'] <- 'cve'
 df_windsp$cve<-as.character(df_windsp$cve)
 df_REDMET<-left_join(df_REDMET, df_windsp, by = c("date","cve"))
 
-#Atmospheric pressure___________________________________________________________________________________________________#
+#Atmospheric pressure______________________________________________________________________________________________#
 setwd("C:/Users/Iván/Dropbox/MEXICO_TEMPERATURE_MODEL/DATA_CLEANING/tmpcleaning/data/ATM_PRESSURE")
 temp = list.files(pattern="*PA.xls")
 myfiles = lapply(temp, read_excel)
@@ -95,7 +81,7 @@ colnames(df_bar.mean)[colnames(df_bar.mean) == 'variable'] <- 'cve'
 df_bar.mean$cve<-as.character(df_bar.mean$cve)
 df_REDMET<-left_join(df_REDMET, df_bar.mean, by = c("date","cve")) 
 
-#Precipitation___________________________________________________________________________________________________#
+#Precipitation_____________________________________________________________________________________________________#
 setwd("C:/Users/Iván/Dropbox/MEXICO_TEMPERATURE_MODEL/DATA_CLEANING/tmpcleaning/data/REDDA")
 temp = list.files(pattern="*PPH.xls")
 myfiles = lapply(temp, read_excel)
@@ -113,7 +99,7 @@ df_REDMET<-left_join(df_REDMET, df_rain, by = c("date","cve"))
 
 rm(df, df_temp, df2, df_r.humidity, df3, df_windsp, df4, df_bar.mean, df5, df_rain)
 
-# Geographic Coordinates of REDMET stations___________________________________________________________________________________________________#
+# Geographic Coordinates of REDMET stations________________________________________________________________________#
 cat_estacion <- read_excel("C:/Users/Iván/Dropbox/MEXICO_TEMPERATURE_MODEL/DATA_CLEANING/tmpcleaning/data/REDMET/cat_estacion.xlsx", 
                            col_types = c("text", "text", "numeric", "numeric", "numeric", "skip", "numeric"))
 colnames(cat_estacion)[colnames(cat_estacion) == 'cve_estac'] <- 'cve'
@@ -130,7 +116,7 @@ cat_estacion$Y<-round(cat_estacion$latitude,digits = 2)
 cat_estacion$ESTADO<-as.character("MCMA")
 cat_estacion <- cat_estacion[, c(1,6,7:9,2,5,4,3)]
 
-# Joins Adar's object with REDMET data___________________________________________________________________________________________________#
+# Joins Adar's object with REDMET data_____________________________________________________________________________#
 options(scipen = 50)
 df_REDMET<-merge(df_REDMET,cat_estacion, by="cve")
 df_REDMET_01_15<-subset(df_REDMET, date>"2001-01-01" & date<"2015-12-31")
@@ -142,3 +128,37 @@ rm(cat_estacion, df_REDMET_01_15)
 
 saveRDS(df_SMN_REDMET, file = "C:/Users/Iván/Dropbox/MEXICO_TEMPERATURE_MODEL/DATA_CLEANING/tmpcleaning/data/SMN_REDMET_2015_barmean_rain.rds")
 
+
+####################################################################################################################
+
+# Data cleaning from the SMN
+# Get data from Onservatories
+library(readr)
+library(data.table)
+library(tidyverse)
+#library(MESS)
+#library(naniar)
+
+x = read_csv_chunked("C:/Users/Iván/Dropbox/MEXICO_TEMPERATURE_MODEL/DATA_CLEANING/tmpcleaning/data/SMN_2018_individual_files/OBS_2018/OBS_HLY.CSV",
+                     chunk_size = 1e5,
+                     DataFrameCallback$new(function(d, pos) 
+                                           d[!is.na(d[["YEAR-MONTH-DAY"]]) &
+                                               d[["YEAR-MONTH-DAY"]] >= "2003-01-01",]))
+setDT(x)
+unique(x$`ELEMENT-CODE`)
+#We only need code 101 for temperature
+x <- x[ which(`ELEMENT-CODE`==101),]
+x <- x %>% select(-contains("FLAG"))
+y <- x[,-c(1,3)]
+y$`YEAR-MONTH-DAY` <- as.character(y$`YEAR-MONTH-DAY`)
+colnames(y)[colnames(y) =='YEAR-MONTH-DAY'] <- 'date'
+y[y==-99999 | y>=50]<-NA
+y$non_count <- apply(y[,c(3:26)], 1, function(x) sum(!is.na(x)))
+
+conditional <- function(x) {
+  if (sum(!is.na(x[3:26])) >= 18) {
+    c(max(x[3:26], na.rm = T), min(x[3:26], na.rm = T), mean(x[3:26], na.rm = T))
+  } else {
+    `c(NA, NA, NA)`
+  }
+}
